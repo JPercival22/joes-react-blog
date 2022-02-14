c0f5e7cf6f25f1e04efd962b7a7e23fc532486f7
Joseph Percival <joseph.m.percivaL@gmail.com>
2022-02-14 01:15:34 +0000

    optimised the code by removing the blogs from the home page and externalised it into its own resuable component and initialised the title and content using props

--- joes-react-blog/src/Home.js
+++ joes-react-blog/src/Home.js
@@ -1,22 +1,18 @@
 import { useState } from 'react';
+import BlogList from './BlockList';
 
 const Home = () => {
     // This useState Hook re-renders the component once it is updated by the user,
     // in this case by clicking on the button, the state REACTS
     const [blogs, setBlogs] = useState([
-        {title:"My New Website", body: "lorem impsum ...", author: "Joe", id: 1},
-        {title:"Welcome Party!", body: "lorem impsum ...", author: "Dan", id: 2},
-        {title:"UX Design Top Tips", body: "lorem impsum ...", author: "Joe", id: 3}
+        { title: "My New Website", body: "lorem impsum ...", author: "Joe", id: 1 },
+        { title: "Welcome Party!", body: "lorem impsum ...", author: "Dan", id: 2 },
+        { title: "UX Design Top Tips", body: "lorem impsum ...", author: "Joe", id: 3 }
     ]);
 
     return (
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>{blog.author}</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} title="All Blogs!"/>
         </div>
     );
 }
