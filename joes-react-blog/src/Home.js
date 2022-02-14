8e84c827b943a13e58db2d6dc092b62e00be22f5
Joseph Percival <joseph.m.percivaL@gmail.com>
2022-02-14 02:36:46 +0000

    fetched the data and parsed it from the endpoints to the screen

--- joes-react-blog/src/Home.js
+++ joes-react-blog/src/Home.js
@@ -4,30 +4,27 @@ import BlogList from './BlockList';
 const Home = () => {
     // This useState Hook re-renders the component once it is updated by the user,
     // in this case by clicking on the button, the state REACTS
-    const [blogs, setBlogs] = useState([
-        { title: "My New Website", body: "lorem impsum ...", author: "Joe", id: 1 },
-        { title: "Welcome Party!", body: "lorem impsum ...", author: "Dan", id: 2 },
-        { title: "UX Design Top Tips", body: "lorem impsum ...", author: "Joe", id: 3 }
-    ]);
-
+    const [blogs, setBlogs] = useState(null);
     const [name, setName] = useState("Joe");
-    const handleDelete = (id) => {
-        // this doesn't Mutate(change) the original Blog state AnimationPlaybackEvent, it just updates it with new array on user interaction (click delete button etc)
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
 
     useEffect(() => {
         // useEffect will always run on every iteration (button click or refresh)
         // By calling the name variable as ana argument inside of the function you can
         // specify you want it runs 
-        console.log("use effect ran");
-        console.log(name);
-    }, [name]);
+        fetch("http://localhost:8000/blogs") //end point returns with a promise
+        .then(res => { //once we get the response from the endpoint (not data yet!)
+            return res.json(); //this the turns the data into a JS object
+        })
+        .then((data) => { //once the json has returned asynchronously (takes time) returns the object(array)
+            console.log(data); //shows the data
+            setBlogs(data)
+        })
+    }, []);
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+            {/* && is conditional exchange in JS, basically if it reads the left and its true, it loads that and doesn't even read the right hand side of the statement and vice versa */}
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
             <button onClick={() => setName("J.Percival")}>Change Name</button>
             <p>{ name }</p>        
         </div>
