5a994fc4a59c307d425c5c770ea0ab955e2edb4c
Joseph Percival <joseph.m.percivaL@gmail.com>
2022-02-14 00:58:05 +0000

    added logic for the blog comp[onent and added styles

--- joes-react-blog/src/Home.js
+++ joes-react-blog/src/Home.js
@@ -1,9 +1,24 @@
+import { useState } from 'react';
+
 const Home = () => {
+    // This useState Hook re-renders the component once it is updated by the user,
+    // in this case by clicking on the button, the state REACTS
+    const [blogs, setBlogs] = useState([
+        {title:"My New Website", body: "lorem impsum ...", author: "Joe", id: 1},
+        {title:"Welcome Party!", body: "lorem impsum ...", author: "Dan", id: 2},
+        {title:"UX Design Top Tips", body: "lorem impsum ...", author: "Joe", id: 3}
+    ]);
+
     return (
         <div className="home">
-            <h2>Homepage</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>{blog.author}</p>
+                </div>
+            ))}
         </div>
     );
 }
- 
+
 export default Home;
